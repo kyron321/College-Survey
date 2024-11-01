f9be44e38e9225df9897f677099f63c3e80ae126
Kyron <kyron@Kyrons-MacBook-Pro-2.local>
2024-11-01 19:53:27 +0000

    feat: add theme support for meta description

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?php echo get_bloginfo('description'); ?>">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
     <?php wp_head(); ?>
     <?php echo '<!-- Header loaded -->'; ?>
