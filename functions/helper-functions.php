01eb052406d2aa05627f8ac97b11fbba1a7896ab
Kyron <kyron@Kyrons-MacBook-Pro-2.local>
2024-11-01 21:14:00 +0000

    feat: remove default post type

--- functions/helper-functions.php
+++ functions/helper-functions.php
@@ -18,11 +18,27 @@ add_action('save_post', 'set_college_state_taxonomy');
 
 function add_theme_supports() {
     add_theme_support('menus');
-    add_theme_support('title-tag');
+    remove_post_type_support('post', 'editor');
 }
 add_action('after_setup_theme', 'add_theme_supports');
 
-add_action('after_setup_theme', 'add_theme_supports');
+function remove_default_post_type($args, $postType) {
+    if ($postType === 'post') {
+        $args['public']                = false;
+        $args['show_ui']               = false;
+        $args['show_in_menu']          = false;
+        $args['show_in_admin_bar']     = false;
+        $args['show_in_nav_menus']     = false;
+        $args['can_export']            = false;
+        $args['has_archive']           = false;
+        $args['exclude_from_search']   = true;
+        $args['publicly_queryable']    = false;
+        $args['show_in_rest']          = false;
+    }
+
+    return $args;
+}
+add_filter('register_post_type_args', 'remove_default_post_type', 0, 2);
 
 function register_main_menu() {
     register_nav_menu('main-menu', __('Main Menu'));
