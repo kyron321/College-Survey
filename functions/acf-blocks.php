6382785060b8b466e3b4221fdff7939b5089b7b9
Kyron <kyron@Kyrons-MacBook-Pro-2.local>
2024-11-01 14:49:31 +0000

    refactor: remove redundant code

--- functions/acf-blocks.php
+++ functions/acf-blocks.php
@@ -31,16 +31,5 @@ function register_acf_blocks() {
             },
         ));
     }
-    //Add Options page
-    if( function_exists('acf_add_options_page') ) {
-        acf_add_options_page(array(
-            'page_title'    => 'Site Options',
-            'menu_title'    => 'Site Options',
-            'menu_slug'     => 'site-options',
-            'capability'    => 'create_users',
-            'redirect'      => false,
-            'position'      => 2
-        )); 
-    }
 }
 add_action('acf/init', 'register_acf_blocks');
