926d82da230c8b688ea03c0dbb22154539e0dd51
Kyron <kyron@Kyrons-MacBook-Pro-2.local>
2024-11-01 21:06:05 +0000

    feat: add accordion to state filters

--- blocks/colleges.php
+++ blocks/colleges.php
@@ -1,18 +1,20 @@
-<?php 
+<?php
 // Get the ACF fields
 $hide_filters = get_field('hide_filters');
 $hide_search = get_field('hide_search');
 $hide_sort_by = get_field('hide_sort_by');
 ?>
 
-<?php if (!$hide_search): ?>
-    <?php get_template_part('template-parts/blocks/colleges/search'); ?>
-<?php endif; ?>
+<section class="colleges">
+    <?php if (!$hide_search): ?>
+        <?php get_template_part('template-parts/blocks/colleges/search'); ?>
+    <?php endif; ?>
 
-<?php if (!$hide_filters): ?>
-    <?php get_template_part('template-parts/blocks/colleges/filters'); ?>
-<?php endif; ?>
+    <?php if (!$hide_filters): ?>
+        <?php get_template_part('template-parts/blocks/colleges/filters'); ?>
+    <?php endif; ?>
 
-<?php if (!$hide_sort_by): ?>
-    <?php get_template_part('template-parts/blocks/colleges/sort-by'); ?>
-<?php endif; ?>
+    <?php if (!$hide_sort_by): ?>
+        <?php get_template_part('template-parts/blocks/colleges/sort-by'); ?>
+    <?php endif; ?>
+</section>
--- template-parts/blocks/colleges/filters.php
+++ template-parts/blocks/colleges/filters.php
@@ -15,15 +15,20 @@ $taxonomies = get_taxonomies(array('public' => true), 'objects');
                     ?>
                     <?php if (!empty($terms) && !is_wp_error($terms)): ?>
                         <div class="filter-group">
-                            <label><?php echo esc_html($taxonomy->label); ?></label>
-                            <?php foreach ($terms as $term): ?>
-                                <div class="checkbox">
-                                    <label for="<?php echo esc_attr($taxonomy->name . '_' . $term->slug); ?>">
-                                        <input type="checkbox" name="<?php echo esc_attr($taxonomy->name); ?>[]" id="<?php echo esc_attr($taxonomy->name . '_' . $term->slug); ?>" value="<?php echo esc_attr($term->slug); ?>">
-                                        <?php echo esc_html($term->name); ?>
-                                    </label>
-                                </div>
-                            <?php endforeach; ?>
+                            <input type="checkbox" id="toggle-<?php echo esc_attr($taxonomy->name); ?>" class="accordion-toggle">
+                            <label for="toggle-<?php echo esc_attr($taxonomy->name); ?>" class="accordion-label">
+                                <?php echo esc_html($taxonomy->label); ?>
+                            </label>
+                            <div class="accordion-content">
+                                <?php foreach ($terms as $term): ?>
+                                    <div class="checkbox">
+                                        <label for="<?php echo esc_attr($taxonomy->name . '_' . $term->slug); ?>">
+                                            <input type="checkbox" name="<?php echo esc_attr($taxonomy->name); ?>[]" id="<?php echo esc_attr($taxonomy->name . '_' . $term->slug); ?>" value="<?php echo esc_attr($term->slug); ?>">
+                                            <?php echo esc_html($term->name); ?>
+                                        </label>
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
                         </div>
                     <?php endif; ?>
                 <?php endif; ?>
