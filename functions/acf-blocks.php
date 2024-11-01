7315e1787ee5d2baf231e2e62d77e0a7c8146aad
Kyron <kyron@Kyrons-MacBook-Pro-2.local>
2024-11-01 14:56:59 +0000

    feat: setup automatic block building

--- functions/acf-blocks.php
+++ functions/acf-blocks.php
@@ -1,5 +1,6 @@
 <?php
-function register_acf_blocks() {
+function register_acf_blocks()
+{
     // Check if function exists and hook into setup.
     if (function_exists('acf_register_block_type')) {
         // Register a banner block.
@@ -7,11 +8,11 @@ function register_acf_blocks() {
             'name'              => 'banner',
             'title'             => __('Banner'),
             'description'       => __('A custom banner block.'),
-            'render_callback'   => 'render_banner_block',
+            'render_callback'   => 'theme_block_render_callback',
             'category'          => 'formatting',
             'icon'              => 'admin-comments',
             'keywords'          => array('banner', 'custom'),
-            'enqueue_assets'    => function(){
+            'enqueue_assets'    => function () {
                 wp_enqueue_style('banner-block', get_template_directory_uri() . '/blocks/banner.css', array(), '1.0.0');
                 wp_enqueue_script('banner-block', get_template_directory_uri() . '/blocks/banner.js', array(), '1.0.0', true);
             },
@@ -21,15 +22,25 @@ function register_acf_blocks() {
             'name'              => 'two-column',
             'title'             => __('Two Column'),
             'description'       => __('A custom two column block.'),
-            'render_callback'   => 'render_two_column_block',
+            'render_callback'   => 'theme_block_render_callback',
             'category'          => 'formatting',
             'icon'              => 'admin-comments',
             'keywords'          => array('two column', 'custom'),
-            'enqueue_assets'    => function(){
+            'enqueue_assets'    => function () {
                 wp_enqueue_style('two-column-block', get_template_directory_uri() . '/blocks/two-column.css', array(), '1.0.0');
                 wp_enqueue_script('two-column-block', get_template_directory_uri() . '/blocks/two-column.js', array(), '1.0.0', true);
             },
         ));
     }
 }
+
+// Automatically include block template files.
+function theme_block_render_callback($block)
+{
+    $slug = str_replace('acf/', '', $block['name']);
+    if (file_exists(get_theme_file_path("/blocks/{$slug}.php"))) {
+        include(get_theme_file_path("/blocks/{$slug}.php"));
+    }
+}
+
 add_action('acf/init', 'register_acf_blocks');
