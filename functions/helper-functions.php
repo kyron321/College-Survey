b14c2a6dda2284af64560cc490d9ef5bbb9c62e5
Kyron <kyron@Kyrons-MacBook-Pro-2.local>
2024-11-01 16:43:56 +0000

    feat: set taxonomy term automatically

---
+++ functions/helper-functions.php
@@ -0,0 +1,17 @@
+<?php
+function set_college_state_taxonomy($post_id) {
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+        return;
+    }
+
+    if ('college' != get_post_type($post_id)) {
+        return;
+    }
+
+    $state = get_field('state', $post_id);
+
+    if ($state) {
+        wp_set_post_terms($post_id, array($state), 'state');
+    }
+}
+add_action('save_post', 'set_college_state_taxonomy');
