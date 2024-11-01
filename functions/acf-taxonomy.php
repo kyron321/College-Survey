18d6b035e0cb5be928f3138dfb137c9bb5672c3c
Kyron <kyron@Kyrons-MacBook-Pro-2.local>
2024-11-01 13:43:16 +0000

    feat: add state taxonomy

---
+++ functions/acf-taxonomy.php
@@ -0,0 +1,29 @@
+<?php
+function register_state_taxonomy() {
+    $labels = array(
+        'name'              => _x('States', 'taxonomy general name', 'textdomain'),
+        'singular_name'     => _x('State', 'taxonomy singular name', 'textdomain'),
+        'search_items'      => __('Search States', 'textdomain'),
+        'all_items'         => __('All States', 'textdomain'),
+        'parent_item'       => __('Parent State', 'textdomain'),
+        'parent_item_colon' => __('Parent State:', 'textdomain'),
+        'edit_item'         => __('Edit State', 'textdomain'),
+        'update_item'       => __('Update State', 'textdomain'),
+        'add_new_item'      => __('Add New State', 'textdomain'),
+        'new_item_name'     => __('New State Name', 'textdomain'),
+        'menu_name'         => __('States', 'textdomain'),
+    );
+
+    $args = array(
+        'hierarchical'      => false,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array('slug' => 'state'),
+    );
+
+    register_taxonomy('state', array('college'), $args);
+}
+
+add_action('init', 'register_state_taxonomy');
