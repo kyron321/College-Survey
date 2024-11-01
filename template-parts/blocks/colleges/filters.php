3df79279384820f98b7df4a3cb7a52133b8d9b0d
Kyron <kyron@Kyrons-MacBook-Pro-2.local>
2024-11-01 20:53:39 +0000

    feat: add basic state taxonomy filter

--- template-parts/blocks/colleges/filters.php
+++ template-parts/blocks/colleges/filters.php
@@ -1,1 +1,34 @@
-filters
+<?php
+$taxonomies = get_taxonomies(array('public' => true), 'objects');
+?>
+
+<?php if (!empty($taxonomies)): ?>
+    <div class="filters">
+        <form>
+            <?php foreach ($taxonomies as $taxonomy): ?>
+                <?php if ($taxonomy->name === 'state'): ?>
+                    <?php
+                    $terms = get_terms(array(
+                        'taxonomy' => $taxonomy->name,
+                        'hide_empty' => false,
+                    ));
+                    ?>
+                    <?php if (!empty($terms) && !is_wp_error($terms)): ?>
+                        <div class="filter-group">
+                            <label><?php echo esc_html($taxonomy->label); ?></label>
+                            <?php foreach ($terms as $term): ?>
+                                <div class="checkbox">
+                                    <label for="<?php echo esc_attr($taxonomy->name . '_' . $term->slug); ?>">
+                                        <input type="checkbox" name="<?php echo esc_attr($taxonomy->name); ?>[]" id="<?php echo esc_attr($taxonomy->name . '_' . $term->slug); ?>" value="<?php echo esc_attr($term->slug); ?>">
+                                        <?php echo esc_html($term->name); ?>
+                                    </label>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            <button type="submit"><?php echo __('Apply Filters', 'textdomain'); ?></button>
+        </form>
+    </div>
+<?php endif; ?>
