3394dc8959b89fe1a40f7df2a08fb619e63e7cb5
Kyron <kyron@Kyrons-MacBook-Pro-2.local>
2024-11-01 20:34:45 +0000

    feat: add basic two column block

---
+++ blocks/two-column.php
@@ -0,0 +1,20 @@
+<?php
+// Get the ACF fields
+$paragraph = get_field('paragraph');
+$image = get_field('image');
+$flip_block = get_field('flip_block');
+$image_to_text_ratio = get_field('image_to_text_ratio');
+?>
+
+<div class="two-column-block<?php echo $flip_block ? ' '.'flip' : ''; ?> ratio-<?php echo esc_attr($image_to_text_ratio); ?>">
+    <div class="column column-text">
+        <?php if ($paragraph): ?>
+            <p><?php echo esc_html($paragraph); ?></p>
+        <?php endif; ?>
+    </div>
+    <div class="column column-image">
+        <?php if ($image): ?>
+            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+        <?php endif; ?>
+    </div>
+</div>
