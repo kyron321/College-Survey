72e02dc3ed6b704c0f2b43e12b2b0fc221a467e1
Kyron <kds.060902@gmail.com>
2024-11-02 02:08:11 +0000

    feat: styling to admin

--- functions/theme-assets.php
+++ functions/theme-assets.php
@@ -5,3 +5,28 @@ function theme_enqueue_assets() {
     wp_enqueue_script('theme-scripts', get_template_directory_uri() . '/dist/js/main.js', [], false, true);
 }
 add_action('wp_enqueue_scripts', 'theme_enqueue_assets');
+
+function enqueue_custom_admin_styles() {
+    wp_enqueue_style('custom-admin-style', get_template_directory_uri() . '/dist/css/admin.css');
+}
+add_action('admin_enqueue_scripts', 'enqueue_custom_admin_styles');
+
+function custom_login_logo() {
+    // Get the site logo URL from ACF options
+    $site_logo = get_field('site_logo', 'option'); // Adjust the field name and options page as needed
+
+    if ($site_logo) {
+        $logo_url = esc_url($site_logo['url']);
+        ?>
+        <style type="text/css">
+            body.login h1 a {
+                background-image: url('<?php echo $logo_url; ?>');
+                background-size: 100%;
+                height: 84px;
+                width: 100%;
+            }
+        </style>
+        <?php
+    }
+}
+add_action('login_enqueue_scripts', 'custom_login_logo');
