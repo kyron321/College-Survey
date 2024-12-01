ef2979ef3dc97a81c6ac343e7b853bc27e9d70fd
Tracy Smith <tracysmith@Tracys-MacBook-Pro.local>
2024-12-01 15:42:51 +0000

    chore: responsive styling

--- template-parts/blocks/colleges/sort-by.php
+++ template-parts/blocks/colleges/sort-by.php
@@ -1,7 +1,7 @@
 <div class="sort-by">
     <form method="GET" action="">
         <label for="sort_order">Sort By:</label>
-        <select name="sort_order" id="sort_order">
+        <select name="sort_order" id="sort_order" class="styled-dropdown">
             <option value="a-z">Alphabetical (A-Z)</option>
             <option value="z-a">Alphabetical (Z-A)</option>
         </select>
