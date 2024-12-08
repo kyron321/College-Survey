1b21cebfbf9632f4709bc1bc9744882491d52ac8
Kyron <tfkyron@gmail.com>
2024-12-08 16:41:32 +0000

    Tidying: Format colleges block

--- blocks/colleges.php
+++ blocks/colleges.php
@@ -7,14 +7,14 @@ $hide_sort_by = get_field('hide_sort_by');
 
 <section class="colleges">
     <div class="column-one">
-        <h3>Filters<h3>
-                <?php if (!$hide_search): ?>
-                    <?php get_template_part('template-parts/blocks/colleges/search'); ?>
-                <?php endif; ?>
+        <h3>Filters</h3>
+        <?php if (!$hide_search): ?>
+        <?php get_template_part('template-parts/blocks/colleges/search'); ?>
+        <?php endif; ?>
 
-                <?php if (!$hide_filters): ?>
-                    <?php get_template_part('template-parts/blocks/colleges/filters'); ?>
-                <?php endif; ?>
+        <?php if (!$hide_filters): ?>
+        <?php get_template_part('template-parts/blocks/colleges/filters'); ?>
+        <?php endif; ?>
     </div>
 
     <div class="modal" id="filtersModal">
@@ -23,7 +23,7 @@ $hide_sort_by = get_field('hide_sort_by');
         </button>
         <h3>Filters</h3>
         <?php if (!$hide_filters): ?>
-            <?php get_template_part('template-parts/blocks/colleges/filters-mobile'); ?>
+        <?php get_template_part('template-parts/blocks/colleges/filters-mobile'); ?>
         <?php endif; ?>
     </div>
     <div class="modal-overlay" id="modalOverlay"></div>
@@ -33,13 +33,11 @@ $hide_sort_by = get_field('hide_sort_by');
 
     <div class="column-two">
         <?php if (!$hide_sort_by): ?>
-            <?php get_template_part(
-                'template-parts/blocks/colleges/sort-by'
-            ); ?>
+        <?php get_template_part('template-parts/blocks/colleges/sort-by'); ?>
         <?php endif; ?>
         <div class="search-section">
             <?php if (!$hide_search): ?>
-                <?php get_template_part('template-parts/blocks/colleges/search'); ?>
+            <?php get_template_part('template-parts/blocks/colleges/search'); ?>
             <?php endif; ?>
         </div>
         <div class="college-info-container">
@@ -50,14 +48,14 @@ $hide_sort_by = get_field('hide_sort_by');
             <p class="presence">
                 Freedom
                 <i class="fa fa-info-circle tooltip-icon" aria-hidden="true"></i>
-                <span class="tooltip-text">A rating for each college, ranging from Excellent to Moderate to Poor, based
-                    on how free it is from trans ideology.</span>
+                <span class="tooltip-text">
+                    A rating for each college, ranging from Excellent to Moderate to Poor, based
+                    on how free it is from trans ideology.
+                </span>
             </p>
             <p class="more-info"></p>
         </div>
 
-        <?php get_template_part(
-            'template-parts/blocks/colleges/colleges-list'
-        ); ?>
+        <?php get_template_part('template-parts/blocks/colleges/colleges-list'); ?>
     </div>
 </section>
