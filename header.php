b94ef39db863b96105b4f294701202a806784e78
Kyron <tfkyron@gmail.com>
2024-12-19 19:06:49 +0000

    Remove title tag, handled by yoast

--- header.php
+++ header.php
@@ -5,17 +5,13 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<?php echo get_bloginfo(
-        'description'
-    ); ?>">
+                                            'description'
+                                        ); ?>">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
-    <title><?php
-    wp_title('|', true, 'right');
-    bloginfo('name');
-    ?></title>
     <?php wp_head(); ?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body <?php body_class(); ?>>
 
-<?php get_template_part('template-parts/header/template', 'header'); ?>
+    <?php get_template_part('template-parts/header/template', 'header'); ?>
